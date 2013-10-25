b2edad2aee14155ef123dc33a5f14d8d28f5b70c
George <George@George-PC.lan>
2013-10-25 21:59:24 +0100

    Added first python files

---
+++ complex.py
@@ -0,0 +1,24 @@
+'''
+Created on 24 Oct 2013
+
+@author: George
+'''
+
+class Complex:
+    '''
+    classdocs
+    '''
+    
+
+    def __init__(self, rePart,imPart):
+                self.Re = rePart
+                self.Im = imPart
+    
+    def square(self):
+        result_Real = self.Re**2 + (self.Im**2)*(-1)
+        result_Imaginary = 2*self.Re*self.Im
+        finalResult = Complex(result_Real,result_Imaginary)
+        return finalResult
+    
+    def modulus(self):
+        return (self.Re**2 + self.Im**2)**0.5
---
+++ main.py
@@ -0,0 +1,27 @@
+'''
+Created on 22 Oct 2013
+
+@author: George
+'''
+
+import png
+from complex import *
+from decimal import *
+
+# Setup
+#getcontext().prec = 28
+
+# def evaluatePixel(xCoord,yCoord):
+#     curr_zn_ = 0 # The variable that will be looped through the formula
+#     count = 0 # No. of iterations
+#     while curr_zn <=4:
+#         
+# def iterateMandelbrot(zn):
+#     
+# def squareComplex(re,im):
+#   
+#   
+#test = Complex(Decimal('1.021'),Decimal('1.223453'))
+#test_Square = test.square()
+
+#print "result is: " + str(test_Square.Re) + "+" + str(test_Square.Im) + "i" 
