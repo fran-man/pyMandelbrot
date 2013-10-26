f0c4567d855a267e5ce1814ff4972728dbd22624
George <George@George-PC.lan>
2013-10-26 07:50:24 +0100

    Adding code to convert pixel to complex

--- main.py
+++ main.py
@@ -9,8 +9,18 @@ from complex import *
 from decimal import *
 from pixArray import *
 
-# Setup
+# Setup Defaults
 #getcontext().prec = 28
+# Mandelbrot World Parameters
+xMin = -2
+xMax = 1
+yMin = -0.845
+yMax = 0.845
+xRange = abs(xMin) + abs(xMax)
+yRange = abs(yMin) + abs(yMax)
+
+imgWidth = 300
+scaleFactor = imgWidth/xRange 
 
 # Start with a 300x300 image
 
@@ -18,9 +28,18 @@ imgSource = pixArray(100,100)
 
 for i in range(0,100):
     imgSource.setPixel(i, i, 2*i, 0, 0)
-    print str(i)
+    #print str(i)
 
 png.from_array(imgSource.getRaw(), 'RGB').save('a.png')
+
+def pixToIm(pixel_X,pixel_Y):
+    '''
+    Convert a pixel's xy coordinate into
+    a complex number with decimal coefficients
+    '''
+    img_X = pixel_X/scaleFactor + xMin # Div by scaleFactor maps to range [0,xRange].
+    img_Y = pixel_Y/scaleFactor + yMin
+    return Complex(img_X,img_Y)
 # def evaluatePixel(xCoord,yCoord):
 #     curr_zn_ = 0 # The variable that will be looped through the formula
 #     count = 0 # No. of iterations
