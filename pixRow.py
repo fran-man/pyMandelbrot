50f010506f20815cb5b03bf208741876030cbd83
George <George@George-PC.lan>
2013-10-29 20:42:27 +0000

    added pixrow wrapper to interact with individual row

---
+++ pixRow.py
@@ -0,0 +1,22 @@
+'''
+Created on 25 Oct 2013
+
+@author: George
+'''
+
+class pixRow(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self,xSize):
+        self.Array = [0 for x in range(xSize*3)]
+    
+    def getRaw(self):
+        return self.Array
+    
+    def setPixel(self,xVal,yVal,red,green,blue):
+        self.Array[3*xVal] = red
+        self.Array[3*xVal + 1] = green
+        self.Array[3*xVal + 2] = blue
